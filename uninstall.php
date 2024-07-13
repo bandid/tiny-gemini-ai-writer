24963d09feb3beb61ad9b1d6f666db2d9bab925d
Daniel Bandi <dannimation@gmail.com>
2024-07-13 03:03:24 +0200

    Add files via upload
    Added uninstall file

---
+++ uninstall.php
@@ -0,0 +1,8 @@
+<?php
+// If uninstall.php is not called by WordPress, die.
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+
+// Delete the stored API key from the database
+delete_option('gemini_ai_api_key');
